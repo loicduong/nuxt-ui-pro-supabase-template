17e72f3fd37c5748205ea3b57b004e4ae5045377
locdp <locdp@suga.com.vn>
2025-06-30 11:55:49 +0700

    feat: add supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,8 @@ export default defineNuxtConfig({
     '@nuxt/ui-pro',
     '@nuxt/eslint',
     '@nuxt/image',
-    '@nuxthub/core'
+    '@nuxthub/core',
+    '@nuxtjs/supabase'
   ],
 
   devtools: {
